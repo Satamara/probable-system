3fd1cba55c1a56c23f8a1984d4722cb962d27b63
Satamara <95866848+Satamara@users.noreply.github.com>
2022-01-27 17:57:05 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//function creatBlock() {
+  //  for (let i =1; i<=2; i++){
+    //const box = document.querySelector('box')
+   // const newBlock = document.createElement('div')
+   // newBlock.classList = ('mini')
+   // }
+
+//}
+function creatBlock() {
+    for (let i =1; i<=2; i++){
+    const box = document.querySelector('box')
+    const newBlock = document.createElement('div')
+    newBlock.classList = ('mini')
+    }
+ box.append(newBlock)
+}
